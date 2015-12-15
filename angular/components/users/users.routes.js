abb4c94deae7e519ecdbab8692978e88779a220a
Marco Túlio Ávila <marco@localhost.localdomain>
2015-12-15 19:31:21 -0200

    [add] rotas de edição e profile em usuários
    Foram adicionadas as rotas para editar um usuário e para visualizar
    um perfil. Foi adicionado também uma parte de funções resolve para
    os resolves das rotas.

--- angular/components/users/users.routes.js
+++ angular/components/users/users.routes.js
@@ -15,7 +15,8 @@
                 url: '/users',
                 views: {
                     'content': {
-                        template: '<div ui-view="content"></div>'
+                        template: '<div ui-view="content"></div>',
+                        controller: 'UsersController as parent'
                     }
                 }
             })
@@ -36,6 +37,52 @@
                         templateUrl: 'users/views/new.html'
                     }
                 }
+            })
+            .state('app.users.edit', {
+                url: '/{id:int}/edit',
+                resolve: {
+                    user: resolveUser
+                },
+                views: {
+                    'content': {
+                        controller: 'UserEditController as vm',
+                        templateUrl: 'users/views/edit.html',
+                    }
+                }
+            })
+            .state('app.users.profile', {
+                url: '/{id:int}',
+                resolve: {
+                    user: resolveUser
+                },
+                views: {
+                    'content': {
+                        templateUrl: 'users/views/profile.html',
+                        controller: 'UserProfileController as vm',
+                    }
+                }
             });
+    }
+
+    ///////////////////////
+    // RESOLVE FUNCTIONS //
+    ///////////////////////
+
+    resolveUser.$inject = ['$timeout', 'cfpLoadingBar'];
+    function resolveUser($timeout, cfpLoadingBar) {
+        cfpLoadingBar.start();
+        cfpLoadingBar.inc();
+        var user = {
+            id: 3,
+            firstName: 'Polyana',
+            lastName: 'Souza',
+            email: 'polyanafsouza@gmail.com',
+            imageUrl: 'img/profile1.jpg',
+            profile: 'USER',
+        };
+        return $timeout(function() {
+            cfpLoadingBar.complete();
+            return user;
+        }, 1000);
     }
 })();
